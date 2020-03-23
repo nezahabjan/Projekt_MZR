607fc0f4eac1c4999cb441a2a752fa7c62b6732a
nezahabjan <nezahabjan123@gmail.com>
2020-03-23 15:01:05 +0100

    shiny aplikacija

--- shiny/ui.R
+++ shiny/ui.R
@@ -14,8 +14,10 @@ server <- function(input, output, session) {
     
     if (input$dir == "yes"){
       output$text_1 <- renderText({ "Graf bo usmerjen, torej doloci se stevilo oglisc!" })
-    } else {
+    } else if (input$dir == "no"){
       output$text_1 <- renderText({ "Graf bo neusmerjen, torej doloci dimenziji povezavne matrike!" }) 
+    } else {
+      output$text_1 <- renderText({ "Preden nadaljujes moras dolociti usmerjenost grafa!" }) 
     }
   })
   
@@ -23,25 +25,16 @@ server <- function(input, output, session) {
   observeEvent( input$generate, {
     
     if (input$generate == "yes"){
-      output$text_2 <- renderText({ " Le se en klik te loci od dobljenega grafa... " })
+      output$text_2 <- renderText({ "Super, podati mi moras le izbrane atribute grafa!" })
+    } else if (input$generate == "no"){
+      output$text_2 <- renderText({ "Sedaj moras vnesti se povezavno matriko svojega grafa :)" })
     } else {
-      output$text_2 <- renderText({ " Sedaj moras vnesti se povezavno matriko svojega grafa :) " })
+      output$text_2 <- renderText({ "Prosim, izpolni zgornje polje!" })
     }
       
   })
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
   
   observeEvent( input$button1, {
     
@@ -54,7 +47,7 @@ server <- function(input, output, session) {
       })
       
     
-    } else {
+    } else if (input$generate == "no"){
 
       matrika <- as.matrix(lapply(1:input$Dim_x, function(i) 
         as.numeric(unlist(strsplit(input[[paste0("v",i)]])))))
@@ -87,28 +80,46 @@ body <- dashboardBody(
             fluidRow(sidebarPanel(
               h3("Dobrodosli v svetu grafov!"),
               
-              selectInput("dir", "Izberi usmerjenost grafa",
-                          choices = list("yes" = "yes", "no" = "no")),
               
-              textOutput("text_1"),
-              
-              numericInput(inputId = "Dim_x",
-                           label = "Izberi x dimenzijo matrike",
-                           value = 4 ),
-              
-              numericInput(inputId = "Dim_y",
-                           label = "Izberi y dimenzijo matrike",
-                           value = 5 ),
-              
-              numericInput(inputId = "ogl",
-                           label = "Izberi stevilo oglisc grafa",
-                           value = 6 ),
-              
-              selectInput("generate", "Zelis, da ti ponudim graf?",
-                          choices = list("no" = "no","yes" = "yes")),
+              selectInput("generate", "Zelis, da ti ponudim nakljucen graf po izbranih atributih?",
+                          choices = list("no" = "no","yes" = "yes", "izberi"="izberi"), selected = "izberi"),
               
               textOutput("text_2"),
               
+              
+              selectInput("dir", "Izberi usmerjenost grafa",
+                          choices = list("yes" = "yes", "no" = "no", "nevem" = "nevem"), selected = "nevem"),
+              
+              textOutput("text_1"),
+              
+              
+          conditionalPanel(
+                condition = "input.dir == 'yes' || input.generate == 'yes'",
+                numericInput(inputId = "ogl",
+                             label = "Izberi stevilo oglisc grafa",
+                             value = 0 )
+                ),
+          
+          conditionalPanel(
+            condition = "input.dir == 'no'",
+            numericInput(inputId = "Dim_x",
+                         label = "Izberi x dimenzijo matrike",
+                         value = 0 ),
+            
+            numericInput(inputId = "Dim_y",
+                         label = "Izberi y dimenzijo matrike",
+                         value = 0 )
+          ),
+
+          
+          conditionalPanel(
+            condition = "input.generate == 'no'",
+            lapply((1:2), function(i) {
+              textInput(paste0("v",i), paste0("v",i), "0,1,2")})
+
+          ),
+              
+          
               actionButton("button1", "Submit")
               
             ),
