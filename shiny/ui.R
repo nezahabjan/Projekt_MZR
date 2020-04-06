e8bb92563dd50a4384ab27f1d78626a4377eed90
nezahabjan <nezahabjan123@gmail.com>
2020-04-06 07:51:35 +0200

    urejanje skripte

--- shiny/ui.R
+++ shiny/ui.R
@@ -1,4 +1,20 @@
-source("../libraries/lib.R")
+#source("../libraries/lib.R")
+
+#knjiznice, ki jih potrebujemo
+library(igraph)
+library(ggraph)
+library(networkD3)
+library(shiny)
+library(shinydashboard)
+library(shinyjs)
+library(shinyMatrix)
+library(TSP)
+library(pracma)
+library(randomGLM)
+library(googleVis)
+
+
+#datoteke, na katere se sklicujemo v aplikaciji
 source("../glavne funkcije/funkcije.R")
 source("server.R")
 
